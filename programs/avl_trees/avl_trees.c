485ca11649a33c320549f57d2f6899940ccf00c3
Amol Vengurlekar <amol.1991@gmail.com>
2014-06-22 00:28:01 -0500

    AVL trees added. height will updates during recurssion exit. The avl balancing rotation onle one case done.

---
+++ programs/avl_trees/avl_trees.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <tree.h>
+
+#define SIZEOF(x) sizeof(x)/sizeof(x[0])
+
+int main(void)
+{
+	int i;
+	int data[] = {2, 1, 3,6, 8};
+	
+	node_t *disp_array[50];
+	for(i=0; i<SIZEOF(disp_array); i++)
+	{
+		disp_array[i] = NULL;
+	}
+	
+	node_t *new=NULL, *ROOT=NULL;
+	
+	for(i=0; i<SIZEOF(data); i++)
+	{
+		new = createNode(data[i]);
+		if(!new)
+		{
+			printf("cannot create node");
+			exit(1);
+		}
+		if(ROOT==NULL)
+		{
+			//only first pass comes here
+			ROOT = new;
+		}
+		else
+		{
+			attachLeaf(new, ROOT);
+		}
+		printf("\n\n");
+	}
+	
+	treeToArray(disp_array, ROOT->parent, 1, SIZEOF(disp_array));
+	dispTreeArray(disp_array, SIZEOF(disp_array));
+	printf("\n\n");
+	
+	/*rotate(ROOT, LEFT);
+	ROOT = ROOT->parent;
+	for(i=0; i<SIZEOF(disp_array); i++)
+	{
+		disp_array[i] = NULL;
+	}
+	treeToArray(disp_array, ROOT, 1, SIZEOF(disp_array));
+	dispTreeArray(disp_array, SIZEOF(disp_array));
+	*/
+	printf("\n\n%d\n", (ROOT->parent)->height);
+	return 0;
+}
